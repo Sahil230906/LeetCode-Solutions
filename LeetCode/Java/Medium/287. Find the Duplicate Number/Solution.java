38248eb86c28f58ce5781c2751bfd6d616bcb369
Sahil230906 <sahilj2309@gmail.com>
2026-08-12 20:47:02 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/287. Find the Duplicate Number/Solution.java
+++ LeetCode/Java/Medium/287. Find the Duplicate Number/Solution.java
@@ -1,13 +1,23 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < nums.length; i++) {
-            while(set.contains(nums[i])) {
-                return nums[i];
-            }
-            set.add(nums[i]);
+        // Phase 1: Find the meeting point
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find the duplicate
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+
+        return slow;
     }
 }
