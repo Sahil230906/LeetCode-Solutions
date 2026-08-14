5eaf3bff6ddd71806bb20eb0a8973ffdc076b2ff
Sahil230906 <sahilj2309@gmail.com>
2026-08-14 18:36:03 +0530

    Add Java Solution to Patterns/Sliding Window

--- LeetCode/Patterns/Sliding Window/209. Minimum Size Subarray Sum/Solution.java
+++ LeetCode/Patterns/Sliding Window/209. Minimum Size Subarray Sum/Solution.java
@@ -1,19 +1,22 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-     int left = 0;
-     int minLen = Integer.MAX_VALUE;
-     int sum = 0;
+          int left = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
 
-     for(int right = 0; right < nums.length; right++) {
-        sum += nums[right];
-        while(sum >= target) {
-            minLen = Math.min(minLen, right - left + 1);
-            sum = sum - nums[left];
-            left++;
+        for(int right = 0; right < nums.length; right++) {
+            sum = sum + nums[right];
+
+                while(sum >= target) {
+                    min = Math.min(min, right - left + 1);
+
+                    sum = sum - nums[left];
+                    left++;
+                }
+            
         }
-    if(minLen == Integer.MAX_VALUE) return 0;
+        if(min == Integer.MAX_VALUE) return 0;
 
-     }  
-        return minLen;
+        return min;
     }
 }
