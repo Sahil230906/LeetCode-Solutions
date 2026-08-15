d6de1ece2bd34b46f73180882553c26fac26cce5
Sahil230906 <sahilj2309@gmail.com>
2026-08-15 18:04:35 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/1288. Maximum Subarray Sum with One Deletion/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/1288. Maximum Subarray Sum with One Deletion/Solution.java
@@ -2,19 +2,19 @@ class Solution {
     public int maximumSum(int[] arr) {
 
         int noDelete = arr[0];
-        int oneDelete = Integer.MIN_VALUE / 2;
+        int oneDelete = 0;
         int answer = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
 
             int newNoDelete = Math.max(
-                arr[i],
-                noDelete + arr[i]
+                noDelete + arr[i],
+                arr[i]
             );
 
             int newOneDelete = Math.max(
-                noDelete,
-                oneDelete + arr[i]
+                oneDelete + arr[i],
+                noDelete
             );
 
             noDelete = newNoDelete;
