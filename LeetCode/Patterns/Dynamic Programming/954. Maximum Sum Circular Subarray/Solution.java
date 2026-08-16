110e139e88b3a84574a0030b087bcb7341c4ecc6
Sahil230906 <sahilj2309@gmail.com>
2026-08-16 16:06:51 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/954. Maximum Sum Circular Subarray/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/954. Maximum Sum Circular Subarray/Solution.java
@@ -1,30 +1,29 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
         int totalSum = nums[0];
-
         int maxEnding = nums[0];
-        int minEnding = nums[0];
-
         int maxSum = nums[0];
         int minSum = nums[0];
+        int minEnding = nums[0];
 
-        for (int i = 1; i < nums.length; i++) {
+        for(int i = 1; i < nums.length; i++) {
             totalSum += nums[i];
 
-            maxEnding = Math.max(nums[i], maxEnding + nums[i]);
-            minEnding = Math.min(nums[i], minEnding + nums[i]);
+            int v1 = nums[i];
+            int v2 = maxEnding + nums[i];
+            int v3 = minEnding + nums[i];
+
+            maxEnding = Math.max(v1,v2);
+            minEnding = Math.min(v1,v3);
 
             maxSum = Math.max(maxSum, maxEnding);
             minSum = Math.min(minSum, minEnding);
         }
-
-        // All elements are negative
-        if (maxSum < 0) {
+        if(maxSum < 0) {
             return maxSum;
         }
-
         int circularSum = totalSum - minSum;
 
-        return Math.max(maxSum, circularSum);
+        return Math.max(maxSum,circularSum);
     }
 }
